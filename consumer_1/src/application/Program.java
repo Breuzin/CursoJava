f1a56610e9cf93cb059ac21a4205aadd73d4b2b1
Breuzin <guimine.20@gmail.com>
2022-02-20 14:33:48 -0300

    Interface implementation

---
+++ consumer_1/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		// Consumer: functional interface with type
+		// generic <T>, where have an unique method
+		// accept().
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 1000.00));
+		list.add(new Product("Phone", 900.00));
+		list.add(new Product("HeadSet", 300.00));
+		list.add(new Product("Computer", 3000.00));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
+	}
+
+}
