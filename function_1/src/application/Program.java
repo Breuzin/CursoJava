0cc298fe38befaaf8a7ca0447ce4dba50a9b56e5
Breuzin <guimine.20@gmail.com>
2022-02-21 08:30:22 -0300

    Inline lambda implementation (function)

--- function_1/src/application/Program.java
+++ function_1/src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -18,8 +17,7 @@ public class Program {
 		list.add(new Product("Iphone", 4000.00));
 		list.add(new Product("Mouse", 40.00));
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
